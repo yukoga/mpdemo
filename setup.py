c8622a138e84a39f98dd84823a83bccc7e30bc8a
yukoga <yukoga@gmail.com>
2017-02-23 11:38:47 +0900

    add update feature to Spreadsheet class.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='mpdemo',
-    version='0.1.0',
+    version='0.1.1',
     description='Google Analytics Measurement Protocol helper library.',
     license='no',
     author='yukoga',
